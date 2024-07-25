5369a349037907ed9e013f7ba7f537fc268abcbb
alice <alicemarinhogms@gmail.com>
2024-07-24 21:56:09 -0300

    Estruturando as classes

--- Data.java
+++ Data.java
@@ -189,12 +189,4 @@ public class Data {
         return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
     }
 
-    public static void main(String[] args) {
-        Data data = new Data(23, 7, 2024);
-        System.out.println("Data formatada 1: " + data.mostra1());
-        System.out.println("Data formatada 2: " + data.mostra2());
-        System.out.println("Ano bissexto: " + data.bissexto());
-        System.out.println("Dias transcorridos: " + data.diasTranscorridos());
-        data.apresentaDataAtual();
-    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
-import java.util.Scanner;
 
-public static void main(String[] args) {
-   
+public class Main {
+
+     public static void main(String[] args) {
+        Data data = new Data(23, 7, 2024);
+        System.out.println("Data formatada 1: " + data.mostra1());
+        System.out.println("Data formatada 2: " + data.mostra2());
+        System.out.println("Ano bissexto: " + data.bissexto());
+        System.out.println("Dias transcorridos: " + data.diasTranscorridos());
+        data.apresentaDataAtual();
+    }
 }
+   
